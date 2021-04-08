a167d20ec8386e6572864e98f247c4ceef4acbdd
pikachub2005 <74213212+pikachub2005@users.noreply.github.com>
2021-04-08 17:59:14 -0400

    scratchapi.py

--- scratchapi.py
+++ scratchapi.py
@@ -1,49 +1,51 @@
 import os, json, requests, multiprocessing
+os.system('npm install scratch-api')
+import scratchapi
 
-os.system('pip install scratchapi')
+def runJS():
+  os.system('node cloud.js')
 
 class Scratch:
-	def __init__(username,password):
-		s=scratchapi.ScratchUserSession(username, password)
-		if not s.tools.verify_session():
-			raise Exception('Login Failed.')
-		self.username=username
-		self.password=password
-		self.Log={}
-	def runJS():
-  		os.system("Cloud.js")
-	def GetVars(self, ProjID):
-		OldLog = self.Log
-		try:
-			self.Log = json.loads(requests.get("https://clouddata.scratch.mit.edu/logs?projectid="+str(self.ProjID)+"&limit=1000&offset=0").text)
-		except:
-			self.Log = OldLog
-		self.vars = {}
-		for x in range(0, len(self.Log)):
-			y = self.Log[x]
-			if not "☁ " in str(y["value"]) and not y["name"][2:] in self.vars:
-				self.vars.update({y["name"][2:]: y["value"]})
-		return self.vars
-	def GetVar(self,ProjID,VarName):
-		return GetVars(self, ProjID)[VarName]
-	def GetMessages(self,user):
-		s.users.get_message_count(user)
-	def SetVar(self,ProjID,VarName,VarValue):
-		with open("communicate.txt", "w") as file:
-			file.write(str(self.ProjId)+"\n"+self.username+"\n"+self.password+"\n☁ "+varName+"\n"+str(varValue))
-		process = multiprocessing.Process(target=runJS)
-		process.start()
-		while not str(self.GetVar(varName)) == str(varValue):
-			pass
-		process.terminate()
-	def Follow(self,user):
-		s.users.follow(user)
-	def Unfollow(self,user)
-		s.users.unfollow(user)
-	class Comment:
-		def Profile(self,user,comment):
-			s.users.comment(user,comment)
-		def Studio(self,StudioID,comment):
-			s.studios.comment(StudioID,comment)
-		def Project(ProjID,comment):
-			s.projects.comment(ProjID,comment)
+  def __init__(self,username,password):
+    s=scratchapi.ScratchUserSession(username, password)
+    if not s.tools.verify_session():
+      raise Exception('Login Failed.')
+    self.username=username
+    self.password=password
+    self.Log=[]
+  def GetVars(self, ProjID):
+    OldLog = self.Log
+    try:
+      self.Log = json.loads(requests.get("https://clouddata.scratch.mit.edu/logs?projectid="+str(ProjID)+"&limit=1000&offset=0").text)
+    except:
+      self.Log = OldLog
+    self.vars = {}
+    for x in range(len(self.Log)):
+      y = self.Log[x]
+      if not "☁ " in str(y["value"]) and not y["name"][2:]in self.vars:
+        self.vars.update({y["name"][2:]: y["value"]})
+    return self.vars
+  def GetVar(self,ProjID,VarName):
+    info=self.GetVars(ProjID)
+    return info[VarName]
+  def GetMessages(self,user):
+    self.s.users.get_message_count(user)
+  def SetVar(self,ProjID,VarName,VarValue):
+    with open("communicate.txt", "w") as file:
+      file.write(str(ProjID)+"\n"+self.username+"\n"+self.password+"\n☁ "+VarName+"\n"+str(VarValue))
+    process = multiprocessing.Process(target=runJS)
+    process.start()
+    while not str(self.GetVar(ProjID,VarName)) == str(VarValue):
+      pass
+    process.terminate()
+  def Follow(self,user):
+    self.s.users.follow(user)
+  def Unfollow(self,user):
+    self.s.users.unfollow(user)
+  class Comment:
+    def Profile(self,user,comment):
+      self.s.users.comment(user,comment)
+    def Studio(self,StudioID,comment):
+      self.s.studios.comment(StudioID,comment)
+    def Project(self,ProjID,comment):
+      self.s.projects.comment(ProjID,comment)
